5479182a5e5c49c087d2ee99e764f3a4897a8c1f
Dmitry <Dmitry>
2022-12-19 00:40:47 +0300

    1.1

---
+++ lab6/1.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+extern char **environ;
+
+int main(int argc, char *argv[]) {
+    int cnt = 0;
+    char **ptr = environ;
+    while (*ptr++)
+        cnt++;
+    printf("Number of environment variables: %d\n", cnt);
+
+    return 0;
+}
