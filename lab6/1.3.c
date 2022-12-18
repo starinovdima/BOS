60a45cf9c7762dc88ad1253a5577cbf366eacf63
Dmitry <Dmitry>
2022-12-19 00:41:54 +0300

    1.3

---
+++ lab6/1.3.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+extern char **environ;
+
+int main(int argc, char *argv[]) {
+    char **ptr;
+    for (ptr = environ; *ptr && ptr - environ < 10; ptr++)
+        printf("%s\n", *ptr);
+    
+    return 0;
+}
