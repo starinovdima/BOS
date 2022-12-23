1ccf3bf31f56ddae7dbfbb08bd5ba4970067dbb4
Dmitry <Dmitry>
2022-12-24 00:49:10 +0300

    remake 3.4

--- lab4/scripts/3.4_script.sh
+++ lab4/scripts/3.4_script.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
-
-
-if [ $# -eq 0 ] 
-then
-	PWD=$(pwd)
-else
-	PWD=$1
-fi
-
-echo "--- catalogs ---"
-echo ""
-ls -lAF $PWD | grep '/$'
-echo ""
-echo "--- usual files ---"
-echo ""
-ls -lAF $PWD | grep '^-'
-echo ""
-echo "--- symbol references ---"
-echo ""
-ls -lAF $PWD | grep '^l'
-echo ""
-echo "--- symbol devices ---"
-echo ""
-ls -lAF $PWD | grep '^c'
-echo ""
-echo "--- block devices ---"
-echo ""
-ls -lAF $PWD | grep '^b'
-echo ""
+echo "Каталоги:"
+ls -l "$1" | grep ^d
+echo "Обычные файлы:"
+ls -l "$1" | grep ^-
+echo "Символьные ссылки:"
+ls -l "$1" | grep ^l
+echo "Символьные устройства:"
+ls -l "$1" | grep ^c
+echo "Блочные устройства:"
+ls -l "$1" | grep ^b
