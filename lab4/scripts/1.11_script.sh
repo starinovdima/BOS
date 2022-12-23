955dc517d0b1cc4a2b752012f652a59aa1379782
Dmitry <Dmitry>
2022-12-24 00:45:35 +0300

    remake 1.11

--- lab4/scripts/1.11_script.sh
+++ lab4/scripts/1.11_script.sh
@@ -1,24 +1,11 @@
-#! /bin/bash
-
-PWD=$(pwd)
-
-echo " catalogs "
-echo ""
-ls -lAF $PWD | grep '/$'
-echo ""
-echo " usual files "
-echo ""
-ls -lAF $PWD | grep '^-'
-echo ""
-echo " symbol references "
-echo ""
-ls -lAF $PWD | grep '^l'
-echo ""
-echo " symbol devices "
-echo ""
-ls -lAF $PWD | grep '^c'
-echo ""
-echo " block devices "
-echo ""
-ls -lAF $PWD | grep '^b'
-echo ""
+#!/bin/bash
+echo "Каталоги:"
+ls -l | grep ^d
+echo "Обычные файлы:"
+ls -l | grep ^-
+echo "Символьные ссылки:"
+ls -l | grep ^l
+echo "Символьные устройства:"
+ls -l | grep ^c
+echo "Блочные устройства:"
+ls -l | grep ^b
